0685cd815e82e896052d2eb799bf61b427afda6d
HOANGLAM2952002 <tommy2952002@gmail.com>
2022-02-09 22:14:11 -0500

    connect client server success

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,51 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Client {
+    private final int PORT = 8080;
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void connectToServer(String hostname, int port) throws IOException {
+        if (hostname.equals("localhost")) hostname = InetAddress.getLocalHost().getHostAddress();
+        socket = new Socket(hostname, port);
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public String sendMessageToServer(String msg) throws IOException {
+        out.println(msg);
+        return in.readLine();
+    }
+
+    public void closeClient() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to the KeyValue Service Client");
+        System.out.println("Enter the IP address or Hostname of the server: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String hostname = scanner.next();
+
+        System.out.println("Please wait while I connect you...");
+        Client client = new Client();
+        client.connectToServer(hostname, client.PORT);
+        System.out.println("Welcome to the KeyValue Service");
+
+        while (true) {
+            System.out.print("KeyValue Service> ");
+            String msg = scanner.next();
+
+            if (msg.equals("bye")) break;
+
+            String res = client.sendMessageToServer(msg);
+            System.out.println(res);
+        }
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,44 @@
+import java.io.*;
+import java.net.*;
+
 public class Server {
+    private final int PORT = 8080;
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private KeyValueStore kvs;
+
+    public void initServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        socket = serverSocket.accept();
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public void sendResponseToClient(String msg) {
+        if (msg.equals("help")) {
+            out.println("Call help");
+        }
+        if (msg.equals("get")) {
+            out.println("Call get");
+        }
+    }
+
+    public void closeServer() throws IOException {
+        in.close();
+        out.close();
+        serverSocket.close();
+        socket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        server.initServer(server.PORT);
+        server.sendResponseToClient(server.in.readLine());
+
+        KeyValueStore kvs = new KeyValueStore();
+        kvs.put("OK", "1");
+        kvs.put("OKOK", "2");
+    }
 }
